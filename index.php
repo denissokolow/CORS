2b04c00e15871e34cc9d9ab781197c4ed1118cb3
Denis Sokolov <denissokolov@MacBook-Pro-Denis.local>
2020-11-17 23:31:55 +0300

    master

--- index.php
+++ index.php
@@ -24,7 +24,6 @@ if (isset($_GET['print']) && isset($_GET['public'])) {
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: text/plain; charset=utf-8');
     header('Access-Control-Allow-Methods: GET, POST, DELETE');
-    echo file_get_contents(basename(__FILE__));
 }else {
     echo "<h1>" . $getDate->nowD() . "</h1>";
 }
